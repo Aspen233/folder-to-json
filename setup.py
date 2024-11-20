b54c55e6d1c4ccd57d796bce27e756ee8bc46fb9
Aspen233 <529852071@qq.com>
2024-11-19 22:34:40 -0500

    Refactored main script, updated setup.py, removed unused files

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'folder2json=main:main',
+            'folder2json=scripts.main:main',
         ],
     },
     install_requires=[],

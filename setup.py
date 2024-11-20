41acc2d1b65fb91d6f869bdc838562e88ed13474
Aspen233 <529852071@qq.com>
2024-11-19 22:25:16 -0500

    Your commit message describing the changes

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'folder-to-json=folder_to_json.main:main',
+            'folder2json=main:main',
         ],
     },
     install_requires=[],
     description='A tool to convert folder structures and file content into JSON format.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author='Your Name',
-    url='https://github.com/yourusername/folder-to-json',
+    author='Aspen233',
+    url='https://github.com/Aspen233/folder-to-json',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
